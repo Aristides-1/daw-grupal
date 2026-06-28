6ba2d08a5d78844e931843ae60f6e2f114571cde
diego <dcervantesapa@unsa.edu.pe>
2026-06-27 23:36:08 -0500

    añadiendo puertos basse

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-vh#b_1p-%btou*8(^^k$)md+72)7@r8%kx*&1w$snstw#^6_n4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
 
 # Application definition
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     
 ]
 AUTH_USER_MODEL = 'usuarios.Usuario'
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
