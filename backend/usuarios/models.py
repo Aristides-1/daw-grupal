4193aa08cc72f92391ce72f3b5e2b989b12a81c2
diego <dcervantesapa@unsa.edu.pe>
2026-06-27 23:07:23 -0500

    añadiendo clase rol de models.py de usuarios

--- backend/usuarios/models.py
+++ backend/usuarios/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Rol(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nombre
