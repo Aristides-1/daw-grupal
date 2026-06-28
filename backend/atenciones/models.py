67fd8488cd866d24c0223e2fdcef2961d6945098
diego <dcervantesapa@unsa.edu.pe>
2026-06-28 00:09:40 -0500

    creando tabla de cita con formulario detalle y relacion de 1 a 1 con cita

--- backend/atenciones/models.py
+++ backend/atenciones/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from citas.models import Cita
 
-# Create your models here.
+class Atencion(models.Model):
+    motivo = models.TextField()
+    sintomas = models.TextField()
+    diagnostico = models.TextField()
+    tratamiento = models.TextField()
+    observaciones = models.TextField(blank=True)
+
+    cita = models.OneToOneField(Cita, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Atención {self.id}"
