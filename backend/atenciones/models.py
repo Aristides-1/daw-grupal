c9b741146b012a99f5c46f880ab595b72fe2ac40
diego <dcervantesapa@unsa.edu.pe>
2026-06-28 00:20:10 -0500

    formulario de atencion actualizado

--- backend/atenciones/models.py
+++ backend/atenciones/models.py
@@ -3,9 +3,7 @@ from citas.models import Cita
 
 class Atencion(models.Model):
     motivo = models.TextField()
-    sintomas = models.TextField()
-    diagnostico = models.TextField()
-    tratamiento = models.TextField()
+    tratamiento = models.TextField(blank=False)
     observaciones = models.TextField(blank=True)
 
     cita = models.OneToOneField(Cita, on_delete=models.CASCADE)
