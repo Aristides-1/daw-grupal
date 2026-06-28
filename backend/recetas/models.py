e38f08ce7ff3d4d9fa16a29cf6b0d4dbd2397465
diego <dcervantesapa@unsa.edu.pe>
2026-06-28 00:22:48 -0500

    añadiendo formulario de receta simple

--- backend/recetas/models.py
+++ backend/recetas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from atenciones.models import Atencion
 
-# Create your models here.
+class Receta(models.Model):
+    medicamentos = models.CharField(max_length=100)
+    indicaciones = models.TextField(blank=False)
+
+    atencion = models.ForeignKey(Atencion, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.medicamento
