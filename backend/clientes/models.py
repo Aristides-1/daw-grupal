4746fb26aefca154c1b7a8e14f724b40fd17845b
diego <dcervantesapa@unsa.edu.pe>
2026-06-27 23:59:29 -0500

    añadiendo modelo de clientes con sus atributos respectivos

--- backend/clientes/models.py
+++ backend/clientes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    documento = models.CharField(max_length=20, unique=True)
+    telefono = models.CharField(max_length=20)
+    correo = models.EmailField()
+    direccion = models.TextField()
+
+    def __str__(self):
+        return f"{self.nombres} {self.apellidos}"
