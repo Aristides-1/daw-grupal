e6b7f67bf3de6bf12a9172cdeaf687a1f53b54ad
diego <dcervantesapa@unsa.edu.pe>
2026-06-28 22:19:52 -0500

    añadiendo view para consumir api rest de veterinario

--- backend/veterinarios/views.py
+++ backend/veterinarios/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Veterinario, Especialidad
+from .serializers import VeterinarioSerializer, EspecialidadSerializer
 
-# Create your views here.
+class VeterinarioViewSet(viewsets.ModelViewSet):
+    queryset = Veterinario.objects.all()
+    serializer_class = VeterinarioSerializer
+
+
+class EspecialidadViewSet(viewsets.ModelViewSet):
+    queryset = Especialidad.objects.all()
+    serializer_class = EspecialidadSerializer
