7cf0fede547a57a625b7b73539abe3549fe5fb26
diego <dcervantesapa@unsa.edu.pe>
2026-06-28 21:54:42 -0500

    añadiendo Api final para rutas globales

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,9 +3,21 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from clientes.views import ClienteViewSet
+from mascotas.views import MascotaViewSet
+from veterinarios.views import VeterinarioViewSet, EspecialidadViewSet
+from citas.views import CitaViewSet
+from atenciones.views import AtencionViewSet
+from recetas.views import RecetaViewSet
 
 router = DefaultRouter()
+
 router.register(r'clientes', ClienteViewSet)
+router.register(r'mascotas', MascotaViewSet)
+router.register(r'veterinarios', VeterinarioViewSet)
+router.register(r'especialidades', EspecialidadViewSet)
+router.register(r'citas', CitaViewSet)
+router.register(r'atenciones', AtencionViewSet)
+router.register(r'recetas', RecetaViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
