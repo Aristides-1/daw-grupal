8fc785cd5c53db17013a07085ed7436a8f3f858a
diego <dcervantesapa@unsa.edu.pe>
2026-06-28 23:01:19 -0500

    agregando imports de jwt y modelo de autentificacion con tokens temporales

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,3 +1,7 @@
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
@@ -22,4 +26,6 @@ router.register(r'recetas', RecetaViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
