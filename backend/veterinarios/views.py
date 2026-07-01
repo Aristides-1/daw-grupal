85568d6e3f56a444e2ee691dac112e1e031727f8
diego <dcervantesapa@unsa.edu.pe>
2026-07-01 18:57:28 -0500

    permisos exclusivos de crear, eliminar, modificar a administrador sobre veterinarios

--- backend/veterinarios/views.py
+++ backend/veterinarios/views.py
@@ -1,11 +1,17 @@
 from rest_framework import viewsets
 from .models import Veterinario, Especialidad
 from .serializers import VeterinarioSerializer, EspecialidadSerializer
+from usuarios.permissions import IsAdmin
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class VeterinarioViewSet(viewsets.ModelViewSet):
     queryset = Veterinario.objects.all()
     serializer_class = VeterinarioSerializer
 
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsAdmin()]
+        return [IsAuthenticated()]
 
 class EspecialidadViewSet(viewsets.ModelViewSet):
     queryset = Especialidad.objects.all()
