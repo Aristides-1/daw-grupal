dc63c88e35f37720e43936e38b8c299003a4b6a6
diego <dcervantesapa@unsa.edu.pe>
2026-07-01 18:56:34 -0500

    exlusividad de eliminacion para rol admin autenticado para un registro de cliente

--- backend/clientes/views.py
+++ backend/clientes/views.py
@@ -1,7 +1,14 @@
 from rest_framework import viewsets
 from .models import Cliente
 from .serializers import ClienteSerializer
+from rest_framework.permissions import IsAuthenticated
+from usuarios.permissions import IsAdmin
 
 class ClienteViewSet(viewsets.ModelViewSet):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
+
+    def get_permissions(self):
+        if self.action == 'destroy':
+            return [IsAdmin()]
+        return [IsAuthenticated()]
