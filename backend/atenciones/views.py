2e36c05339374f8fca1464f62e80fcff8c8bd2ee
diego <dcervantesapa@unsa.edu.pe>
2026-07-01 18:51:29 -0500

    creando funcion para permisos exclusivo de veterinario autenticado

--- backend/atenciones/views.py
+++ backend/atenciones/views.py
@@ -1,7 +1,13 @@
 from rest_framework import viewsets
 from .models import Atencion
 from .serializers import AtencionSerializer
+from usuarios.permissions import IsVeterinario
 
 class AtencionViewSet(viewsets.ModelViewSet):
     queryset = Atencion.objects.all()
     serializer_class = AtencionSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [IsVeterinario()]
+        return [IsAuthenticated()]
