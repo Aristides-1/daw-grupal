b4d17593f82e2db54433f906d266db803265ddb1
diego <dcervantesapa@unsa.edu.pe>
2026-07-01 18:55:34 -0500

    dando permisos exclusivo a veterinario o admin
    La funcion recrea la solicitud de parte del cliente mas no la aprobacion, permite la cita al veterinario de turno o al admin, y finalmente brinda la capacidad de destruccion de registro exclusivo para administrador. Autenticacion base requerida.

--- backend/citas/views.py
+++ backend/citas/views.py
@@ -1,12 +1,21 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-
-from usuarios.permissions import EsVeterinario
+from usuarios.permissions import IsAdminsAdmin, IsVeterinario, IsCliente
 from .models import Cita
 from .serializers import CitaSerializer
-
 
 class CitaViewSet(viewsets.ModelViewSet):
     queryset = Cita.objects.all()
     serializer_class = CitaSerializer
-    permission_classes = [IsAuthenticated, EsVeterinario]
+
+    def get_permissions(self):
+        if self.action == "create":
+            return [IsCliente()]   # cliente SOLO solicita
+
+        if self.action in ["update", "partial_update"]:
+            return [IsVeterinario() | IsAdmin()]  # control médico
+
+        if self.action == "destroy":
+            return [IsAdmin()]  # solo admin elimina
+
+        return [IsAuthenticated()]
