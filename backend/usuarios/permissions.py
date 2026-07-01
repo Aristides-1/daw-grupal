f7a6c4d1c681b3fadd166fb317d76899055207d6
diego <dcervantesapa@unsa.edu.pe>
2026-07-01 17:51:29 -0500

    usuario admin personalizado

--- backend/usuarios/permissions.py
+++ backend/usuarios/permissions.py
@@ -1,9 +1,14 @@
 from rest_framework.permissions import BasePermission
 
+from rest_framework.permissions import BasePermission
+
 class EsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.rol.nombre == "admin"
-
+        return (
+            request.user and
+            request.user.is_authenticated and
+            request.user.is_superuser
+        )
 
 class EsVeterinario(BasePermission):
     def has_permission(self, request, view):
