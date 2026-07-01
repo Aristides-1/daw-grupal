c6ffa77abb5520ac5316cec252b761ff6960f3a6
diego <dcervantesapa@unsa.edu.pe>
2026-07-01 17:52:47 -0500

    usuario veterinario personalizado

--- backend/usuarios/permissions.py
+++ backend/usuarios/permissions.py
@@ -9,11 +9,14 @@ class EsAdmin(BasePermission):
             request.user.is_authenticated and
             request.user.is_superuser
         )
-
-class EsVeterinario(BasePermission):
+    
+class IsVeterinario(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.rol.nombre == "veterinario"
-
+        return (
+            request.user and
+            request.user.is_authenticated and
+            getattr(request.user, "rol", None) == "veterinario"
+        )
 
 class EsCliente(BasePermission):
     def has_permission(self, request, view):
