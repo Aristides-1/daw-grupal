8e36be05273c99f8a4bf82d7b583b36402b8ec4c
diego <dcervantesapa@unsa.edu.pe>
2026-07-02 12:26:26 -0500

    corrigiendo serializadorde usuario para que pueda registrarse personalizadamente

--- backend/usuarios/serializers.py
+++ backend/usuarios/serializers.py
@@ -2,19 +2,25 @@ from rest_framework import serializers
 from .models import Usuario
 
 class UsuarioSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = Usuario
         fields = [
             "id",
             "username",
             "email",
+            "password",
             "is_active",
             "rol"
         ]
-        extra_kwargs = {
-            "password": {"write_only": True}
-        }
 
     def create(self, validated_data):
-        user = Usuario.objects.create_user(**validated_data)
+        password = validated_data.pop("password")
+
+        user = Usuario(**validated_data)
+        user.set_password(password)  # 🔥 CLAVE (hash correcto)
+        user.is_active = True
+        user.save()
+
         return user
