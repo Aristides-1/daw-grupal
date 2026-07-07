b51723ddc50688ae0fbaefe71d362ebbbbfca242
diego <dcervantesapa@unsa.edu.pe>
2026-07-07 13:17:59 -0500

    agregando funcion para verificar correcta fecha de nacimiento

--- backend/mascotas/serializers.py
+++ backend/mascotas/serializers.py
@@ -1,3 +1,5 @@
+
+from django.utils import timezone
 from rest_framework import serializers
 from .models import Mascota
 
@@ -5,3 +7,10 @@ class MascotaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mascota
         fields = '__all__'
+
+    def validate_fecha_nacimiento(self, value):
+        if value > timezone.now().date():
+            raise serializers.ValidationError(
+                "La fecha de nacimiento no puede ser una fecha futura."
+            )
+        return value
