b451829db8f65d8020fddf292d8d757526fc024a
diego <dcervantesapa@unsa.edu.pe>
2026-07-07 13:07:20 -0500

    agregando un estado válido de cita para ser atendida

--- backend/atenciones/serializers.py
+++ backend/atenciones/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import Atencion
 
+
 class AtencionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Atencion
         fields = '__all__'
+
+    def validate_cita(self, value):
+        if value.estado == "cancelada":
+            raise serializers.ValidationError(
+                "No se puede registrar una atención sobre una cita cancelada."
+            )
+        return value
