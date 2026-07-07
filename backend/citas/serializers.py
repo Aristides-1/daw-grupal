84e532bd477a538395eb28cafa6ece663fd865e5
diego <dcervantesapa@unsa.edu.pe>
2026-07-07 10:18:07 -0500

    validamos funcion para crear cita con fecha valida a partir del dia actual en adelante

--- backend/citas/serializers.py
+++ backend/citas/serializers.py
@@ -5,3 +5,10 @@ class CitaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cita
         fields = '__all__'
+
+    #definimos cita real, sin que la cita sea antes del dia actual
+    def validate_fecha(self, value):
+        if value < timezone.now().date():
+            raise serializers.ValidationError()
+
+        return value
