0c6ed1d6c40d98cb2ef2c3b131a82d1cfcd71c7d
diego <dcervantesapa@unsa.edu.pe>
2026-07-07 13:27:23 -0500

    creando funcion para ingreso de DNI real

--- backend/clientes/serializers.py
+++ backend/clientes/serializers.py
@@ -5,3 +5,13 @@ class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
+
+# aseguramos que se ingrese un DNI valido, con 8 digitos numericos
+def validate_documento(self, value):
+       
+        if len(value) != 8 or not value.isdigit():
+            raise serializers.ValidationError(
+                "El DNI debe tener exactamente 8 dígitos numéricos."
+            )
+
+        return value
