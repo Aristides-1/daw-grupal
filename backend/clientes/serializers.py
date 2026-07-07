18340c3ae06583d60f17e967e145b22b8876f99f
diego <dcervantesapa@unsa.edu.pe>
2026-07-07 13:29:05 -0500

    funcion de nombres obligatorios y no acepta blancos

--- backend/clientes/serializers.py
+++ backend/clientes/serializers.py
@@ -6,12 +6,22 @@ class ClienteSerializer(serializers.ModelSerializer):
         model = Cliente
         fields = '__all__'
 
-# aseguramos que se ingrese un DNI valido, con 8 digitos numericos
-def validate_documento(self, value):
-       
-        if len(value) != 8 or not value.isdigit():
+    #aseguramos que se ingrese un DNI valido, con 8 digitos numericos
+    def validate_documento(self, value):
+        
+            if len(value) != 8 or not value.isdigit():
+                raise serializers.ValidationError(
+                    "El DNI debe tener exactamente 8 dígitos numéricos."
+                )
+
+            return value
+
+    #aseguramos que se ingrese un nombre no vacio
+    def validate_nombres(self, value):
+
+        if not value.strip():
             raise serializers.ValidationError(
-                "El DNI debe tener exactamente 8 dígitos numéricos."
+                "Los nombres son obligatorios."
             )
 
         return value
