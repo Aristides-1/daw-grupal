38b4cc6a0bd3aa5425dcb4d2b6993d1bb36960b4
diego <dcervantesapa@unsa.edu.pe>
2026-07-06 22:58:31 -0500

    se comporara veterinario como String y ya no como objeto

--- backend/usuarios/permissions.py
+++ backend/usuarios/permissions.py
@@ -13,8 +13,9 @@ class IsVeterinario(BasePermission):
     def has_permission(self, request, view):
         return (
             request.user.is_authenticated and
-            request.user.rol and
-            request.user.rol.nombre == "veterinario"
+            request.user.rol and 
+            request.user.rol.nombre == "veterinario" #En vez de objeto veterinario, se compara como String el nombre del rol, ya 
+                                                   #que el objeto veterinario no tiene relación con el usuario, sino que es un objeto independiente.
         )
 
 
