4eef186ac0a8af6b3c309ac8e829f9b43620f924
diego <dcervantesapa@unsa.edu.pe>
2026-07-07 10:13:39 -0500

    registro publico, pero todo los metodos https bloqueados para publico

--- backend/usuarios/views.py
+++ backend/usuarios/views.py
@@ -1,7 +1,17 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Usuario
 from .serializers import UsuarioSerializer
+from .permissions import IsAdmin
+
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]  # cualquiera puede registrarse
+        if self.action in ['update', 'partial_update', 'destroy']:
+            return [IsAdmin()]  # solo admin gestiona usuarios existentes
+        return [IsAuthenticated()]
