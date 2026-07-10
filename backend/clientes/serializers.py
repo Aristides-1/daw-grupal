7e25d08475c415ed203f22055345eeb7d83e4c1f
Aristides-1 <dcervantesapa@unsa.edu.pe>
2026-07-09 23:50:28 -0500

    identacion correcta, bugs

--- backend/clientes/serializers.py
+++ backend/clientes/serializers.py
@@ -3,35 +3,31 @@ from .models import Cliente
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Cliente
-        fields = '__all__'
+            model = Cliente
+            fields = '__all__'
 
     #aseguramos que se ingrese un DNI valido, con 8 digitos numericos
     def validate_documento(self, value):
         
-            if len(value) != 8 or not value.isdigit():
-                raise serializers.ValidationError(
+        if len(value) != 8 or not value.isdigit():
+            raise serializers.ValidationError(
                     "El DNI debe tener exactamente 8 dígitos numéricos."
                 )
+        return value
 
-            return value
-
-    #aseguramos que se ingrese un nombre, un apellido y tambien un telefono, que no sean vacios y numerico
-
+    def validate_nombres(self, value):
         if not value.strip():
             raise serializers.ValidationError(
-                "Los nombres son obligatorios."
-            )
+                    "Los nombres son obligatorios."
+                        )
 
         return value
 
     def validate_apellidos(self, value):
-
         if not value.strip():
             raise serializers.ValidationError(
                 "Los apellidos son obligatorios."
             )
-
         return value
 
     def validate_telefono(self, value):
